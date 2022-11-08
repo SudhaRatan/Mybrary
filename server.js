0a9c16925bb073f45f6ca92323347f9eab098d1a
SudhaRatan <rattankumar2511@gmail.com>
2022-11-08 23:49:25 +0530

    initial setup

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ mongoose.connect(process.env.DATABASE_URL,{
     useNewUrlParser:true,
     family:4,
 })
-// const db = mongoose.connection
+const db = mongoose.connection
 
-// db.on('error',error => console.error(error))
-// db.once('open', () => console.log("Connected to mongoose"))
+db.on('error',error => console.error(error))
+db.once('open', () => console.log("Connected to mongoose"))
 
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
